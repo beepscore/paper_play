9e3efa4bb6590dcb158475f77bc83490fb453281
beepscore <support@beepscore.com>
2013-06-27 23:58:49 -0700

    Rename path to line.

--- js/draw.js
+++ js/draw.js
@@ -1,14 +1,14 @@
 // Create a Paper.js Path to draw a line into it:
-var path = new Path();
+var line = new Path();
 // Give the stroke a color
-path.strokeColor = 'black';
-path.strokeWidth = 4;
+line.strokeColor = 'black';
+line.strokeWidth = 4;
 var start = new Point(100, 100);
 // Move to start and draw a line from there
-path.moveTo(start);
+line.moveTo(start);
 // Note the plus operator on Point objects.
 // PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
+line.lineTo(start + [ 100, -50 ]);
 
 // Create a circle shaped path with its center at the center
 // of the view and a radius of 30:
